9772933281f82cf366af20742adfbe32b10c1c48
Vlad Ovčaruk <58884452+AzrailVOI@users.noreply.github.com>
2023-09-14 09:02:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs');
+const https = require("https")
 
 app.get('/', (req, res) => {
     console.log((req.connection.remoteAddress).split(":").pop())

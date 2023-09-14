0401cc3cd0e47adb2ab7715c99367ae2efe9b68e
Vlad Ovčaruk <58884452+AzrailVOI@users.noreply.github.com>
2023-09-14 09:01:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
 
 app.get('/', (req, res) => {
     console.log((req.connection.remoteAddress).split(":").pop())
   res.send('Hello World from Polska NodeJS Server')
 })
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+})*/
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+https.createServer(options, app).listen(port, console.log(`server runs on port ${port}`))
